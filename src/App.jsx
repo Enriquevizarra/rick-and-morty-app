36cbfa736ee5dd7aa805e90a004afe787afbb9f7
Enriquevizarra <vizarramattay@gmail.com>
2023-02-09 19:38:17 -0500

    ADD: functions,useeefect and useStates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,61 @@
+import { useEffect, useState } from 'react'
+import { Pagination } from './components/Pagination'
 import './App.css'
+import { Characters } from './components/Characters'
+import { Navbar } from './components/Navbar'
+import { get } from './data/httpClient'
 
 function App() {
+  const API = "https://rickandmortyapi.com/api/character"
+  const [characters, setCharacters] = useState([])
+  const [info, setInfo] = useState({})
+  
+  useEffect(() => {
+    get(API).then(data => {
+      setCharacters(data.results)
+      setInfo(data.info)
+    })
+  },[])
+
+  const onPrevious = () => { 
+    get(info.prev).then(data => {
+      console.log(data)
+      setCharacters(data.results)
+      setInfo(data.info)
+    })
+  }
+  const onNext = () => {
+      get(info.next).then(data => {
+        console.log(data)
+        setCharacters(data.results)
+        setInfo(data.info)
+      })
+  }
 
   return (
-    <div>yeco</div>
+   <>
+    <Navbar
+      brand="Rick and Morty App"
+    />
+    <div className='container mt-5'>
+      <Pagination 
+        prev={info.prev}
+        next={info.next}
+        onPrevious={onPrevious}
+        onNext={onNext}
+      />
+      
+      <Characters
+        characters={characters}
+      />
+      <Pagination 
+        prev={info.prev}
+        next={info.next}
+        onPrevious={onPrevious}
+        onNext={onNext}
+      />
+    </div>
+   </>
   )
 }
 
